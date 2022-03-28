ec2d597c324b8dd2529bd13dceddf25fcf1b92f7
xqm32 <458173774@qq.com>
2022-03-28 22:51:01 +0800

    fix bug in `main.py`

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import argparse
 import os
+import sys
 
 from utils import HFUTer, log
 
 if __name__ == "__main__":
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("username", type=str, help="Your student ID")
-    arg_parser.add_argument("password", type=str,
-                            help="Password for one.hfut.edu.cn")
-    arg_parser.add_argument("address", type=str, help="Check in address")
-    args = arg_parser.parse_args()
-
-    if args == []:
+    if len(sys.argv) == 1:
         env_dist = os.environ
     else:
+        arg_parser = argparse.ArgumentParser()
+        arg_parser.add_argument("username", type=str, help="Your student ID")
+        arg_parser.add_argument("password", type=str,
+                                help="Password for one.hfut.edu.cn")
+        arg_parser.add_argument("address", type=str, help="Check in address")
+        args = arg_parser.parse_args()
         env_dist = vars(args)
 
     stu = HFUTer(username=env_dist['username'], password=env_dist['password'])
